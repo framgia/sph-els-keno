dc0e29a90963ab2c9824c4337801040bf9de6a87
kenorenzsun <116168014+kenorenzsun@users.noreply.github.com>
2022-11-18 19:58:47 +0800

    [SELS-TASK] Setup Seeders for Models (#2)
    * Create Seeders for dummy data
    
    * Use upsert when to avoid repetition (2nd commit )
    
    * Create new migrations for table changes(3rd commit)
    
    Co-authored-by: Keno Bacunawa <keno@bacunawa.com>

--- els-back/database/seeders/DatabaseSeeder.php
+++ els-back/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            CategorySeeder::class,
+            QuizSeeder::class,
+            WordSeeder::class,
+            ChoiceSeeder::class,
+            UserSeeder::class,
+            AdminSeeder::class
+        ]);
     }
 }
