71063f5e6322887c24c52b97caf267937e126f24
kenorenzsun <116168014+kenorenzsun@users.noreply.github.com>
2022-11-22 14:01:23 +0800

    [SELS-TASK] Setup React Project (#5)
    * Create Seeders for dummy data
    
    * Use upsert when to avoid repetition (2nd commit )
    
    * Create new migrations for table changes(3rd commit)
    
    * Setup env and axios
    
    * Integrate protected route  (2nd commit)
    
    * Resolve comments (3rd commit)
    
    * Remove s (4th commit)
    
    Co-authored-by: Keno Bacunawa <keno@bacunawa.com>

---
+++ els-front/src/plugins/Router/index.js
@@ -0,0 +1,30 @@
+import {
+    useRoutes,
+} from "react-router-dom";
+import Main from "../../components/Main.js";
+import Auth from "../../components/Auth";
+import ProtectedRoute from "../../components/ProtectedRoute.js";
+
+const Routes = () => {
+    let routes = useRoutes([
+        { 
+            path: "/",
+            element: 
+                <ProtectedRoute user={true} redirectPath="/auth/login"> 
+                    <Main />
+                </ProtectedRoute>
+        },
+        { 
+            path: "/auth", 
+            element: 
+                <ProtectedRoute user={true} for_auth redirectPath="/"> 
+                    <Auth /> 
+                </ProtectedRoute>
+        },
+        // ...
+    ]);
+
+    return routes;
+}
+
+export default Routes;
