3c048efd10da67ab9909ac7846f9400cf72164e1
Keno Bacunawa <keno@bacunawa.com>
2022-11-23 15:23:06 +0800

    Install and setup react toaster

--- els-front/src/App.js
+++ els-front/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom"
 import DefaultRoutes from "./plugins/Router/index"
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
