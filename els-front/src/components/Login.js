f6e2d0e33587531a124331052a575b490b3894ff
Keno Bacunawa <keno@bacunawa.com>
2022-11-23 14:35:23 +0800

    Add react final form and integrate to login (1st commit)

--- els-front/src/components/Login.js
+++ els-front/src/components/Login.js
@@ -1,8 +1,82 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Field } from 'react-final-form'
+
 
 const Login = () => {
+    const onSubmit = async (formValues) => {
+        console.log(formValues)
+    } 
+
     return (
-        <div className="text-bold">Login</div>
+        <div className="flex w-[30rem] flex-col space-y-10">
+            <div className="text-center text-4xl font-medium">Log In</div>
+            <Form 
+                onSubmit={ onSubmit }
+                validate= {(formValues) => {
+                    const errors = {};
+
+                    if(!formValues.email) 
+                        errors.email = "Email is required"
+                    else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(formValues.email))
+                        errors.email = "Invalid email address"
+
+                    if(!formValues.password)
+                        errors.password = "Password is required"
+
+                    return errors;
+                }}
+                render={({ handleSubmit, submitting }) => (
+                    <form onSubmit={ handleSubmit } className="flex w-[30rem] flex-col space-y-10">
+                        <Field name="email">
+                            {({ input,meta }) => (
+                                <div className="h-12">
+                                    <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-blue-500">
+                                        <input
+                                            {...input}
+                                            type="text"
+                                            placeholder="Email"
+                                            className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+                                        />
+                                    </div>
+                                    {meta.error && meta.touched && <span className="text-red-300">{meta.error}</span>}
+                                </div>
+                            )}
+                        </Field>
+                        <Field name="password">
+                            {({ input, meta }) => (
+                                <div className="h-12">
+                                    <div className="w-full transform border-b-2 bg-transparent text-lg duration-300 focus-within:border-blue-500">
+                                        <input
+                                            {...input}
+                                            type="password"
+                                            placeholder="Password"
+                                            className="w-full border-none bg-transparent outline-none placeholder:italic focus:outline-none"
+                                        />
+                                    </div>
+                                    {meta.error && meta.touched && <span className="text-red-300">{meta.error}</span>}
+                                </div>
+                                
+                            )}
+                        </Field>
+                        <button 
+                            type="submit" disabled={submitting}
+                            className="transform rounded-sm text-white bg-blue-600 py-2 font-bold duration-300 hover:bg-blue-800"
+                        > LOG IN</button>
+                    </form>
+                )}
+            >
+            </Form>
+            <p className="text-center text-lg">
+                No account?
+                <Link 
+                    to="/auth/register"
+                    className="font-medium pl-1 text-blue-500 underline-offset-4 hover:underline"
+                >
+                    Create One
+                </Link>
+            </p>
+        </div>
     )
 }
 
