1f2a6ac5b984cf2a08b1ac4a149a436511ce1346
Keno Bacunawa <keno@bacunawa.com>
2022-11-23 15:46:10 +0800

    axios interceptors

--- els-front/src/plugins/axios.js
+++ els-front/src/plugins/axios.js
@@ -1,7 +1,47 @@
 import axios from "axios";
+import { errorNotify } from "./toast";
 
 const httpClient = axios.create({
-  baseURL : process.env.REACT_APP_API_UR,
+  baseURL : process.env.REACT_APP_API_URL,
+});
+
+httpClient.interceptors.request.use(function(config) {
+  let getAuthToken = () => localStorage.getItem('token')
+  config.headers.Authorization = `Bearer ${getAuthToken()}`;
+
+  return config;
+})
+
+httpClient.interceptors.response.use((response) => {
+  return response;
+}, (error) => {
+  let messages = []
+  switch (error.response.status) {
+      case 404:
+          // messages.push('nous n'avons pas trouvé cette page')
+
+          break;
+      case 500:
+          messages.push('Internal Server Error.')
+          break;
+      case 503:
+          messages.push('Service unavailable, please check your internet connection to continue.')
+          break;
+      case 401:
+          localStorage.setItem('token', '')
+          // history.push('/auth/login') Reminder to implement this
+          messages.push('You are not logged in. Please login first.')
+          break;
+      case 422:
+          messages.push(error.response.data.message)
+          break;
+      default:
+          messages.push('Opps, something went wrong in processing your request.')
+          break;
+  }
+      
+  messages.forEach(message => errorNotify(message))
+  console.log(messages)
 });
 
 export default httpClient;
