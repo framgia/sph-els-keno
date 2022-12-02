5c4d7489611abc6a50cdb1ed7ab6f27a2fd284a8
Keno Bacunawa <keno@bacunawa.com>
2022-12-02 18:15:16 +0800

    onsubmit remove on form

--- els-front/src/components/UpdateProfile.js
+++ els-front/src/components/UpdateProfile.js
@@ -61,8 +61,8 @@ const UpdateProfile = ({user}) => {
                     onSubmit={ handleUpdate }
                     validate= { validateForm }
                     initialValues={user}
-                    render={({ handleSubmit, submitting }) => (
-                        <form onSubmit={ handleSubmit } className="flex w-[30rem] flex-col space-y-10 mt-5 mx-5">
+                    render={({ submitting }) => (
+                        <form  className="flex w-[30rem] flex-col space-y-10 mt-5 mx-5">
                             <Field name="name">
                                 {({ input,meta }) => (
                                     <div className="h-5">
